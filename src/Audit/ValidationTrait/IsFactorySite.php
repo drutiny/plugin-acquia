d187d261b32ae8d279da7021bc51844c5706dd38
Sean Hamlin <sean.hamlin@acquia.com>
2018-02-01 22:33:49 +1300

    Code style.

--- src/Audit/ValidationTrait/IsFactorySite.php
+++ src/Audit/ValidationTrait/IsFactorySite.php
@@ -7,12 +7,13 @@ trait IsFactorySite {
 
   /**
    * Detect that this site runs on Acquia Cloud Site Factory.
+   *
+   * @param Sandbox $sandbox
+   * @return bool
    */
-  protected function requireIsFactorySite(Sandbox $sandbox)
-  {
+  protected function requireIsFactorySite(Sandbox $sandbox) {
     $stat = $sandbox->drush(['format' => 'json'])->status();
     return strpos($stat['files'], 'sites/g/files') === 0;
   }
-}
 
- ?>
+}
