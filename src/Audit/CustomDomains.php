fd309b5ae0347f6b89aee5ef1841b36c29aa448f
Josh Waihi <josh.waihi@acquia.com>
2018-05-14 10:57:56 +1200

    Ported custom domains check over to api v2

--- src/Audit/CustomDomains.php
+++ src/Audit/CustomDomains.php
@@ -4,33 +4,36 @@ namespace Drutiny\Acquia\Audit;
 
 use Drutiny\Sandbox\Sandbox;
 use Drutiny\Audit;
+use Drutiny\Credential\Manager;
+use Drutiny\Acquia\CloudApiDrushAdaptor;
 
 /**
  * Ensure an environment has custom domains set.
  */
-class CustomDomains extends CloudApiAwareCheck {
+class CustomDomains extends Audit {
+
+  protected function requireCloudApiV2()
+  {
+    return Manager::load('acquia_api_v2');
+  }
 
   /**
    * @inheritdoc
    */
   public function audit(Sandbox $sandbox) {
-    $opts = $sandbox->drush()->getOptions();
 
-    $sitename = $opts['ac-realm'] . ':' . $opts['ac-site'];
+    $environment = CloudApiDrushAdaptor::getEnvironment($sandbox->getTarget());
 
-    // https://cloudapi.acquia.com/v1/sites/realm:mysite/envs/prod/domains.json
-    $domains = $this->api($sandbox, 'sites/' . $sitename . '/envs/' . $opts['ac-env'] . '/domains.json');
-
-    $domains = array_filter($domains, function ($domain) {
+    $domains = array_filter($environment['domains'], function ($domain) {
       // Do not include ELB domains or Acquia default domains.
-      return !(strpos($domain['name'], 'acquia-sites.com') || strpos($domain['name'], 'elb.amazonaws.com'));
+      return !(strpos($domain, 'acquia-sites.com') || strpos($domain, 'elb.amazonaws.com'));
     });
 
     if (empty($domains)) {
       return FALSE;
     }
 
-    $sandbox->setParameter('domains', $domains);
+    $sandbox->setParameter('domains', array_values($domains));
 
     return TRUE;
   }
