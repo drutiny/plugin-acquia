fada8801bac7c1846a4ae8a7436a8473ccfcf549
Brian Schilt <brian.schilt@gmail.com>
2020-09-01 20:32:16 -0700

    Adding inode data.

--- src/Audit/FilesystemAnalysis.php
+++ src/Audit/FilesystemAnalysis.php
@@ -6,60 +6,85 @@ use Drutiny\Sandbox\Sandbox;
 use Drutiny\Credential\Manager;
 use Drutiny\Acquia\CloudApiDrushAdaptor;
 use Drutiny\Acquia\CloudApiV2;
-use Drutiny\Annotation\Param;
+
 
 /**
  * Audit the usage of the filesystem.
- *
- * @Param(
- *  name = "expression",
- *  type = "string",
- *  default = "true",
- *  description = "The expression language to evaludate. See https://symfony.com/doc/current/components/expression_language/syntax.html"
- * )
- * @Param(
- *  name = "not_applicable",
- *  type = "string",
- *  default = "false",
- *  description = "The expression language to evaludate if the analysis is not applicable. See https://symfony.com/doc/current/components/expression_language/syntax.html"
- * )
- * @Param(
- *  name = "unit",
- *  description = "the unit of measurement to describe the volume usage in. E.g. B,M,G,T.",
- *  default = "G"
- * )
  */
-class FilesystemAnalysis extends EnvironmentAnalysis {
+class FilesystemAnalysis extends EnvironmentAnalysis
+{
 
-  /**
-   * @inheritdoc
-   */
-  public function gather(Sandbox $sandbox) {
-    parent::gather($sandbox);
 
-    $unit = $sandbox->getParameter('unit', "G");
+    public function configure()
+    {
+        $this->addParameter(
+            'expression',
+            static::PARAMETER_OPTIONAL,
+            'The expression language to evaluate. See https://symfony.com/doc/current/components/expression_language/syntax.html',
+            'true'
+        );
+        $this->addParameter(
+            'not_applicable',
+            static::PARAMETER_OPTIONAL,
+            'The expression language to evaluate if the analysis is not applicable. See https://symfony.com/doc/current/components/expression_language/syntax.html',
+            'false'
+        );
+        $this->addParameter(
+            'unit',
+            static::PARAMETER_OPTIONAL,
+            'the unit of measurement to describe the volume usage in. E.g. B,M,G,T.',
+            'G'
+        );
+        $this->addParameter(
+            'filesystem',
+            static::PARAMETER_OPTIONAL,
+            'the storage usage information for both disk and inodes.',
+            ''
+        );
 
-    // Report file system disk space usage based on the unit
-    $output = $sandbox->exec("df -B$unit | grep gfs");
+    }
 
-    // Remove all occurrences the storage unit and % from the output.
-    // This will allow the values to be used in conditional expressions.
-    $output = str_replace([$unit,'%'], '', $output);
+    /**
+     * @inheritdoc
+     */
+    public function gather(Sandbox $sandbox)
+    {
+        parent::gather($sandbox);
 
-    list($volume, $capacity, $used, $free, $usage, $mountpoint) = array_values(array_filter(preg_split("/\t|\s/", $output)));
-<<<<<<< HEAD
-    $this->set('filesystem', [
-=======
+        $unit = $this->getParameter('unit', "G");
 
-    $sandbox->setParameter('filesystem', [
->>>>>>> 886cd1b... Added the 'unit' param and adjusted output for expression use.
-      'volume' => $volume,
-      'capacity' => (int)$capacity,
-      'used' => (int)$used,
-      'free' => (int)$free,
-      'percent_used' => (int)$usage,
-      'mountpoint' => $mountpoint,
-      'unit' => $unit,
-    ]);
-  }
+        // Report file system disk space and inode usage.
+        $output = $this->target->getService('exec')->run("df -B$unit | grep gfs && df --inodes | grep gfs");
+        list($disk, $inode) = array_values(explode(PHP_EOL, $output));
+
+        // Remove all occurrences the storage unit and % from the output.
+        // This will allow the values to be used in conditional expressions.
+        $disk = str_replace([$unit,'%'], '', $disk);
+
+        // Parse the usage data into variables.
+        list($disk_volume, $disk_capacity, $disk_used, $disk_free, $disk_usage, $disk_mountpoint) = array_values(array_filter(preg_split("/\t|\s/", $disk)));
+        list($inode_volume, $inode_capacity, $inode_used, $inode_free, $inode_usage, $inode_mountpoint) = array_values(array_filter(preg_split("/\t|\s/", $inode)));
+
+        $this->set(
+          'filesystem', [
+            'disk' => [
+              'volume' => $disk_volume,
+              'capacity' => (int)$disk_capacity,
+              'used' => (int)$disk_used,
+              'free' => (int)$disk_free,
+              'percent_used' => (int)$disk_usage,
+              'mountpoint' => $disk_mountpoint,
+              'unit' => $unit,
+             ],
+            'inode' => [
+              'volume' => $inode_volume,
+              'capacity' => (int)$inode_capacity,
+              'used' => (int)$inode_used,
+              'free' => (int)$inode_free,
+              'percent_used' => (int)$inode_usage,
+              'mountpoint' => $inode_mountpoint,
+            ]
+          ]
+        );
+    }
 }
