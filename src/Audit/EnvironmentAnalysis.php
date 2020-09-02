285048df2354e140a8e01b6003bed2c6a59af0bf
Brian Schilt <brian.schilt@gmail.com>
2020-09-02 14:42:48 -0700

    Fixed closing bracket typo

--- src/Audit/EnvironmentAnalysis.php
+++ src/Audit/EnvironmentAnalysis.php
@@ -60,9 +60,10 @@ class EnvironmentAnalysis extends AbstractAnalysis {
       'environmentId' => $environment_id
     ]));
 
-  if ($app['hosting']['type'] != 'acsf') {
-    $this->set('variables', $client->getEnvironmentsVariables([
-      'environmentId' => $environment_id
-    ]));
+    if ($app['hosting']['type'] != 'acsf') {
+      $this->set('variables', $client->getEnvironmentsVariables([
+        'environmentId' => $environment_id
+      ]));
+    }
   }
 }
