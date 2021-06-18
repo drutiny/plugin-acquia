a0cd719015b3177e124a75fed8a0a91def6f6ff1
Alex Moreno <alejandro.moreno@tdo.es>
2021-06-18 14:09:16 +0100

    DRU-6: Added warning so we can identify when a domain has been added to AC but maybe is not correctly configured.

--- src/Audit/CustomDomains.php
+++ src/Audit/CustomDomains.php
@@ -10,23 +10,53 @@ use Drutiny\Audit;
  */
 class CustomDomains extends Audit {
 
-  /**
-   * @inheritdoc
-   */
-  public function audit(Sandbox $sandbox) {
+	/**
+	 * @inheritdoc
+	 */
+	public function audit(Sandbox $sandbox) {
+		$warn = FALSE;
 
-    $domains = array_filter($this->target['acquia.cloud.environment.domains'], function ($domain) {
-      // Do not include ELB domains or Acquia default domains.
-      return !(strpos($domain, 'acquia-sites.com') || strpos($domain, 'elb.amazonaws.com') || strpos($domain, 'acsitefactory.com'));
-    });
+		foreach ($this->target['acquia.cloud.environment.domains'] as $domain) {
+			if ($this->urlExists($domain) == FALSE) {
+				$warn = TRUE;
+			}
+		}
 
-    if (empty($domains)) {
-      return FALSE;
-    }
+		$this->set('warnings', $warn);
 
-    $this->set('domains', array_values($domains));
+		$domains = array_filter($this->target['acquia.cloud.environment.domains'], function ($domain) {
+			// Do not include ELB domains or Acquia default domains.
+			return !(strpos($domain, 'acquia-sites.com') || strpos($domain, 'elb.amazonaws.com') || strpos($domain, 'acsitefactory.com'));
+		});
 
-    return TRUE;
-  }
+		if (empty($domains)) {
+			return FALSE;
+		} elseif ($warn) {
+			return self::WARNING;
+		}
+
+		$this->set('domains', array_values($domains));
+
+		return TRUE;
+	}
+
+	function urlExists( $url = NULL)
+	{
+		if($url == NULL) return false;
+		$ch = curl_init($url);
+		curl_setopt($ch, CURLOPT_TIMEOUT, 5);
+		curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		// We don't use data for anything, but it's needed for curl_getinfo.
+		$data = curl_exec($ch);
+		$httpcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+		curl_close($ch);
+
+		if($httpcode == 0){
+			return FALSE;
+		} else {
+			return TRUE;
+		}
+	}
 
 }
