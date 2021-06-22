9c9465ca8ca0d0a7401d8e40dfb4794149d4fda1
Alejandro Moreno <alex-moreno@users.noreply.github.com>
2021-06-22 15:04:56 +0100

    Revert "DRU-6: Added warning so we can identify when a domain has been added …"

--- src/Audit/CustomDomains.php
+++ src/Audit/CustomDomains.php
@@ -10,59 +10,23 @@ use Drutiny\Audit;
  */
 class CustomDomains extends Audit {
 
-	/**
-	 * @inheritdoc
-	 */
-	public function audit(Sandbox $sandbox) {
-		$warn = FALSE;
+  /**
+   * @inheritdoc
+   */
+  public function audit(Sandbox $sandbox) {
 
-		foreach ($this->target['acquia.cloud.environment.domains'] as $domain) {
-			if ($this->urlResponds($domain) == FALSE) {
-				$warn = TRUE;
-			}
-		}
+    $domains = array_filter($this->target['acquia.cloud.environment.domains'], function ($domain) {
+      // Do not include ELB domains or Acquia default domains.
+      return !(strpos($domain, 'acquia-sites.com') || strpos($domain, 'elb.amazonaws.com') || strpos($domain, 'acsitefactory.com'));
+    });
 
-		$this->set('warnings', $warn);
+    if (empty($domains)) {
+      return FALSE;
+    }
 
-		$domains = array_filter($this->target['acquia.cloud.environment.domains'], function ($domain) {
-			// Do not include ELB domains or Acquia default domains.
-			return !(strpos($domain, 'acquia-sites.com') || strpos($domain, 'elb.amazonaws.com') || strpos($domain, 'acsitefactory.com'));
-		});
+    $this->set('domains', array_values($domains));
 
-		if (empty($domains)) {
-			return FALSE;
-		} elseif ($warn) {
-			return self::WARNING;
-		}
-
-		$this->set('domains', array_values($domains));
-
-		return TRUE;
-	}
-
-	/**
-	 * Check if a domain responds to a ping.
-	 *
-	 * @param string $url
-	 * @return bool
-	 */
-	function urlResponds( $url = NULL)
-	{
-		if($url == NULL) return false;
-		$ch = curl_init($url);
-		curl_setopt($ch, CURLOPT_TIMEOUT, 5);
-		curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-		// We don't use data for anything, but it's needed to initialise $ch.
-		$data = curl_exec($ch);
-		$httpcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-		curl_close($ch);
-
-		if($httpcode == 0){
-			return FALSE;
-		} else {
-			return TRUE;
-		}
-	}
+    return TRUE;
+  }
 
 }
