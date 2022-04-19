008d62bbd56063a59bdb7b0573ae0ddc3302ae1f
Omkar Pednekar <omkar.pednekar@acquia.com>
2022-04-19 18:46:05 +0530

    DRU-221: Instead of querying each database seperately, fetching all db size info in single query.

--- src/Audit/DatabaseAnalysis.php
+++ src/Audit/DatabaseAnalysis.php
@@ -20,24 +20,28 @@ class DatabaseAnalysis extends EnvironmentAnalysis {
 
     $data = $this->get('databases');
 
-    $this->set('databases', array_map(function ($database) use ($sandbox) {
+    $sql = "SELECT CONCAT(TABLE_SCHEMA, ':', ROUND(SUM(data_length + index_length) / 1024 / 1024, 1)) as size
+            FROM information_schema.tables
+            GROUP BY table_schema;";
+
+    $results = $this->target->getService('drush')->sqlq($sql)->run(function ($output) {
+      return explode("\n", trim($output));
+    });
+    $db_sizes = [];
+    // We will get result like [<db_name>:<size>].
+    // Format and convert it to array.
+    foreach ($results as $result) {
+      $db = explode(":", trim($result));
+      $db_sizes[$db[0]] = $db[1];
+    }
+
+    $this->set('databases', array_map(function ($database) use ($sandbox, $db_sizes) {
       // Extract the machine_name from the db_url.
       $strArray = explode('/',$database['url']);
       $db_machine_name = end($strArray);
 
-      $sql = "SELECT ROUND(SUM(data_length + index_length) / 1024 / 1024, 1) as size
-              FROM information_schema.tables
-              WHERE table_schema='{$db_machine_name}'
-              GROUP BY table_schema;";
-
-      $result = $this->target->getService('drush')->sqlq($sql)->run(function ($output) {
-        return (float) trim($output);
-      });
-
-      //$result = $sandbox->drush()->sqlq($sql);
-
       $database['machine_name'] = $db_machine_name;
-      $database['size'] = $result;
+      $database['size'] = array_key_exists($db_machine_name, $db_sizes) ? $db_sizes[$db_machine_name] : 0;
       return $database;
     }, $data['_embedded']['items']));
 
