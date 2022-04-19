071c1a12a2f3faa009f909ca21c93d4346dc2410
Josh Waihi <josh.waihi@acquia.com>
2022-04-20 09:06:42 +1200

    Allow the CloudAPI creds to be MIA and still use this driver.

--- src/Api/CloudApi.php
+++ src/Api/CloudApi.php
@@ -5,20 +5,33 @@ namespace Drutiny\Acquia\Api;
 use AcquiaCloudApi\AcquiaCloudApi;
 use Drutiny\Acquia\Plugin\AcquiaCloudPlugin;
 use Drutiny\Http\Client;
+use Drutiny\Plugin\PluginRequiredException;
+use Psr\Log\LoggerInterface;
 
 /**
  * Drutiny wrapper to Acquia Cloud API.
  */
 class CloudApi
 {
-    public function __construct(AcquiaCloudPlugin $plugin, Client $client)
+    protected AcquiaCloudApi $client;
+    protected PluginRequiredException $e;
+
+    public function __construct(AcquiaCloudPlugin $plugin, Client $client, LoggerInterface $logger)
     {
-        $creds = $plugin->load();
-        $this->client = new AcquiaCloudApi($creds['key_id'], $creds['secret']);
+        try {
+          $creds = $plugin->load();
+          $this->client = new AcquiaCloudApi($creds['key_id'], $creds['secret']);
+        }
+        catch (PluginRequiredException $e) {
+          $logger->warning($e->getMessage());
+        }
     }
 
     public function getClient()
     {
+        if (!isset($this->client)) {
+          throw new PluginRequiredException('acquia:cloud');
+        }
         return $this->client;
     }
 }
