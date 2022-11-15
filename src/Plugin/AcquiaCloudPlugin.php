023e04d92161d0fe1432a130ce606a1b71a3decf
Josh Waihi <josh.waihi@acquia.com>
2022-11-15 13:46:17 +1300

    Add support to use local Cloud API creds as preference.

--- src/Plugin/AcquiaCloudPlugin.php
+++ src/Plugin/AcquiaCloudPlugin.php
@@ -3,8 +3,35 @@
 namespace Drutiny\Acquia\Plugin;
 
 use Drutiny\Plugin;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
-class AcquiaCloudPlugin extends Plugin {
+class AcquiaCloudPlugin extends Plugin
+{
+    public function __construct(ContainerInterface $container, InputInterface $input, OutputInterface $output)
+    {
+        parent::__construct($container, $input, $output);
+
+        $homedir = $container->getParameter('user_home_dir');
+        $cloud_api_conf = $homedir . '/.acquia/cloud_api.conf';
+
+        if (!file_exists($cloud_api_conf) || !is_readable($cloud_api_conf)) {
+            return;
+        }
+
+        $container->get('logger')->notice("Found .acquia/cloud_api.conf. Cloud API keys will be used from this file.");
+
+        // Re-use the values in cloud_api.conf.
+        $conf = json_decode(file_get_contents($cloud_api_conf), true);
+
+        if (isset($conf['key']) && empty($this->getField('key_id'))) {
+            $this->setField('key_id', $conf['key']);
+        }
+        if (isset($conf['secret']) && empty($this->getField('secret'))) {
+            $this->setField('secret', $conf['secret']);
+        }
+    }
 
     /**
      * {@inheritdoc}
@@ -20,13 +47,11 @@ class AcquiaCloudPlugin extends Plugin {
             'key_id',
             "Your Key ID to connect to the Acquia Cloud API v2 with. To generate an\nAPI access token, login to https://cloud.acquia.com, then visit\nhttps://cloud.acquia.com/a/profile/tokens, and click **Create Token**:",
             static::FIELD_TYPE_CREDENTIAL
-            )
+        )
           ->addField(
-            'secret',
-            'Your API secret to connect to the Acquia Cloud API v2 with:',
-            static::FIELD_TYPE_CREDENTIAL
+              'secret',
+              'Your API secret to connect to the Acquia Cloud API v2 with:',
+              static::FIELD_TYPE_CREDENTIAL
           );
     }
 }
-
- ?>
