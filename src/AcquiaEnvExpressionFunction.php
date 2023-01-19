75755b5355c710d26588d97bb4ef409a1984ebbf
Josh Waihi <josh.waihi@acquia.com>
2023-01-20 10:05:17 +1300

    Fix PHP 8.1 fatal errors.

--- src/AcquiaEnvExpressionFunction.php
+++ src/AcquiaEnvExpressionFunction.php
@@ -5,6 +5,7 @@ namespace Drutiny\Acquia;
 use Drutiny\ExpressionLanguage\Func\FunctionInterface;
 use Drutiny\ExpressionLanguage\Func\ExpressionFunction;
 use Drutiny\Target\TargetInterface;
+use Closure;
 
 /**
  * Returns the environment object from Acquia Cloud API v2.
@@ -18,19 +19,19 @@ class AcquiaEnvExpressionFunction extends ExpressionFunction implements Function
     $this->target = $target;
   }
 
-  public function getName()
+  public function getName(): string
   {
       return 'AcquiaEnv';
   }
 
-  public function getCompiler()
+  public function getCompiler():Closure
   {
       return function () {
         return 'target("acquia.cloud.environment")';
       };
   }
 
-  public function getEvaluator()
+  public function getEvaluator():Closure
   {
       return function ($args) {
           return $this->target['acquia.cloud.environment']->export();
