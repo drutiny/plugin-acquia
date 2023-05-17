e15214867660db0a21dc9bd3a12566cf0da3dbef
Brian Schilt <brian.schilt@gmail.com>
2023-05-17 10:00:38 -0700

    Fixed the AppInfoAnalysis audit so it returns member api data.

--- src/Audit/CloudApiAnalysis.php
+++ src/Audit/CloudApiAnalysis.php
@@ -42,14 +42,14 @@ class CloudApiAnalysis extends AbstractAnalysis {
         $this->set('drush', $this->target['drush']->export());
         $this->set('app', $this->target['acquia.cloud.application']->export());
 
-        foreach ($this->getParameter('calls') as $name => $call) {
+        foreach ($this->get('calls') as $name => $call) {
             if (!is_array($call) || !isset($call['path'])) {
                 $this->logger->error("$name should be an array containing a path and optionally a verb (e.g. GET) or an options array. Skipping.");
                 continue;
             }
             $response = $this->call(...$call);
             
-            if ($this->getParameter('is_legacy')) {
+            if ($this->get('is_legacy')) {
                 $response = ['_embedded' => ['items' => (array) $response]];
             }
             // Use the name of a call to set a token in the audit.
