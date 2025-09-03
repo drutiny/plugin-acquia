552d851d9643eb1aa5fa3a0947fc10e3a63bf887
Josh Waihi <josh.waihi@acquia.com>
2025-09-03 13:57:38 +1200

    Moving policy sources to static localfs versions as CSKB is decomissioned.

--- src/Source/PolicySource.php
+++ src/Source/PolicySource.php
@@ -2,138 +2,40 @@
 
 namespace Drutiny\Acquia\Source;
 
-use Drutiny\Acquia\Api\SourceApi;
 use Drutiny\Attribute\AsSource;
-use Drutiny\Policy;
-use Drutiny\LanguageManager;
-use Drutiny\Policy\Severity;
-use Drutiny\PolicySource\AbstractPolicySource;
+use Drutiny\PolicySource\LocalFs;
 use Drutiny\Settings;
 use Psr\Log\LoggerInterface;
 use Symfony\Component\DependencyInjection\Attribute\Autoconfigure;
-use Symfony\Component\Yaml\Yaml;
+use Symfony\Component\Finder\Finder;
 use Symfony\Contracts\Cache\CacheInterface;
 
-/**
- * Load policies from CSKB.
- */
-#[AsSource(name: 'ACQUIA', weight: -80)]
+#[AsSource(name: 'ACQUIA', weight: -80, cacheable: false)]
 #[Autoconfigure(tags: ['policy.source'])]
-class PolicySource extends AbstractPolicySource {
-  use SourceTrait;
+class PolicySource extends LocalFs
+{
+    public function __construct(
+        protected Finder $finder,
+        protected Settings $settings,
+        protected LoggerInterface $logger,
+        protected CacheInterface $cache,
+        protected AsSource $source
+    ) {
+        $this->name = $source->name;
 
-  protected string $baseUrl;
+        $policy_dir = realpath(__DIR__ . '/../../' . $settings->get('acquia.policy.library.fs'));
 
-  const API_ENDPOINT = 'jsonapi/node/policy';
-
-  public function __construct(
-    protected SourceApi $client,
-    protected LanguageManager $languageManager,
-    AsSource $source,
-    CacheInterface $cache,
-    protected LoggerInterface $logger,
-    Settings $settings,
-    )
-  {
-    $this->baseUrl = $settings->get('acquia.api.base_uri');
-    parent::__construct(source: $source, cache: $cache);
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  protected function doGetList(LanguageManager $languageManager):array
-  {
-    $list = [];
-    $params = $this->getRequestParams();
-    $params['query']['fields[node--policy]'] = 'field_name,field_class,title,drupal_internal__nid';
-    $params['query']['fields[taxonomy_term--drutiny_audit_classes]'] = 'name';
-    $params['query']['include'] = 'field_class';
-
-    $langcode = LanguageMap::fromLanguageManager($languageManager)->toDrutinyLangCode();
-
-    foreach ($this->client->getList($this->getApiPrefix().self::API_ENDPOINT, $params) as $item) {
-      $list[$item['field_name']] = [
-        'uuid' => $item['uuid'],
-        'name' => $item['field_name'],
-        'class' => $item['field_class'][0]['attributes']['name'],
-        'title' => $item['title'],
-        'language' => $langcode,
-        'uri' => $this->baseUrl . 'node/' . $item['drupal_internal__nid'],
-      ];
-    }
-    return $list;
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  protected function doLoad(array $definition):Policy
-  {
-    $query = $this->getRequestParams();
-    $query['query']['include'] = 'field_tags';
-    $endpoint = $this->getApiPrefix().self::API_ENDPOINT.'/'.$definition['uuid'];
-    $response = $this->client->get($endpoint, $query);
-
-    $fields = $response['data']['attributes'];
-    $definition['chart'] = !empty($fields['field_chart']) ? Yaml::parse($fields['field_chart']) : [];
-    $definition['depends'] = !empty($fields['field_depends']) ? Yaml::parse($fields['field_depends']) : [];
-    $definition['parameters'] = !empty($fields['field_parameters']) ? Yaml::parse($fields['field_parameters']) : [];
-    $definition['build_parameters'] = !empty($fields['field_build_parameters']) ? Yaml::parse($fields['field_build_parameters']['value']) : [];
-    $definition['audit_build_info'] = !empty($fields['field_audit_build_info']) ? Yaml::parse($fields['field_audit_build_info']) : [];
-
-    if (isset($definition['parameters']['_chart'])) {
-      unset($definition['parameters']['_chart']);
-    }
-    // Clean up chart definitions.
-    foreach ($definition['chart'] as $key => $chart) {
-      // Remove empty properties allowing defaults to take place.
-      $definition['chart'][$key] = array_filter($chart, function($v) {
-        // Booleans remain but empty strings and null values go.
-        return !((is_string($v) && empty($v)) || is_null($v));
-      });
-    }
-
-    foreach ($response['included'] ?? [] as $include) {
-        if ($include['id'] == $response['data']['relationships']['field_class']['data']['id']) {
-            $definition['class'] = $include['attributes']['name'];
-            break;
+        if (!$policy_dir) {
+          throw new \RuntimeException('Policy directory not found');
         }
+
+        // Ensure the policy directory is available.
+
+        $this->finder
+            ->files()
+            ->depth('<=1')
+            ->in($policy_dir)
+            ->name('*.policy.yml');
     }
-
-    $tags = [];
-    foreach ($response['data']['relationships']['field_tags']['data'] as $relationship) {
-        foreach ($response['included'] as $include) {
-            if ($include['id'] == $relationship['id'] && $include['type'] == $relationship['type']) {
-                $tags[] = $include['attributes']['name'];
-            }
-        }
-    }
-
-    $definition['tags'] = $tags;
-
-    $definition['severity'] = $response['data']['attributes']['field_severity'];
-    $definition['description'] = $response['data']['attributes']['field_description'];
-    $definition['success'] = $response['data']['attributes']['field_success'];
-    $definition['remediation'] = $response['data']['attributes']['field_remediation'] ?? '';
-    $definition['failure'] = $response['data']['attributes']['field_failure'];
-    $definition['type'] = $response['data']['attributes']['field_type'];
-    $definition['warning'] = $response['data']['attributes']['field_warning'] ?? '';
-
-    unset($definition['source']);
-
-    // If severity isn't set, set it to the default value.
-    $definition['severity'] ??= Severity::getDefault()->value;
-
-    if (!is_string($definition['severity'])) {
-      $definition['severity'] = Severity::fromInt($definition['severity'])->value;
-    }
-    elseif (!Severity::has($definition['severity'])) {
-      $this->logger->warning("Severity of '{$definition['severity']}' is not an allowed value in Policy '{$definition['name']}' from {$this->source->name}.");
-      $definition['severity']  = Severity::getDefault()->value;
-    }
-
-    return parent::doLoad($definition);
-  }
 
 }
--- src/Source/ProfileSource.php
+++ src/Source/ProfileSource.php
@@ -2,17 +2,12 @@
 
 namespace Drutiny\Acquia\Source;
 
-use Drutiny\Acquia\Api\SourceApi;
 use Drutiny\Attribute\AsSource;
-use Drutiny\LanguageManager;
-use Drutiny\Policy\Severity;
-use Drutiny\Profile;
 use Drutiny\ProfileFactory;
-use Drutiny\ProfileSource\AbstractProfileSource;
+use Drutiny\ProfileSource\ProfileSourceLocalFs;
 use Drutiny\Settings;
 use Symfony\Component\DependencyInjection\Attribute\Autoconfigure;
-use Symfony\Component\Yaml\Yaml;
-use Symfony\Component\Yaml\Exception\ParseException;
+use Symfony\Component\Finder\Finder;
 use Symfony\Contracts\Cache\CacheInterface;
 
 /**
@@ -20,96 +15,33 @@ use Symfony\Contracts\Cache\CacheInterface;
  */
 #[AsSource(name: 'ACQUIA', weight: -80)]
 #[Autoconfigure(tags: ['profile.source'])]
-class ProfileSource extends AbstractProfileSource
+class ProfileSource extends ProfileSourceLocalFs
 {
     use SourceTrait;
     public const API_ENDPOINT = 'jsonapi/node/profile';
     protected string $baseUrl;
 
     public function __construct(
-      protected SourceApi $client,
-      protected LanguageManager $languageManager,
-      AsSource $source,
-      CacheInterface $cache,
-      ProfileFactory $profileFactory,
-      Settings $settings
-      )
+      protected Finder $finder, 
+      protected Settings $settings, 
+      protected AsSource $source, 
+      protected CacheInterface $cache, 
+      protected ProfileFactory $profileFactory
+    )
     {
-      $this->baseUrl = $settings->get('acquia.api.base_uri');
-      parent::__construct(source: $source, cache: $cache, profileFactory: $profileFactory);
+        $profile_dir = realpath(__DIR__ . '/../../' . $settings->get('acquia.profile.library.fs'));
+
+        if (!$profile_dir) {
+          throw new \RuntimeException('Profile directory not found');
+        }
+
+        // Ensure the profile directory is available.
+
+        $this->finder
+            ->files()
+            ->depth('<=1')
+            ->in($profile_dir)
+            ->name('*.profile.yml');
     }
   
-
-    /**
-     * {@inheritdoc}
-     */
-    protected function doGetList(LanguageManager $languageManager): array
-    {
-        $list = [];
-        $query = $this->getRequestParams();
-        $query['query']['fields[node--profile]'] = 'title,field_name,drupal_internal__nid';
-
-        foreach ($this->client->getList($this->getApiPrefix().self::API_ENDPOINT, $query) as $item) {
-          $list[$item['field_name']] = [
-            'name' => $item['field_name'],
-            'title' => $item['title'],
-            'uuid' => $item['uuid'],
-            'language' => LanguageMap::fromLanguageManager($languageManager)->toDrutinyLangCode(),
-            'uri' =>  $this->baseUrl . 'node/' . $item['drupal_internal__nid']
-          ];
-        }
-        return $list;
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    protected function doLoad(array $definition): Profile
-    {
-        $query = $this->getRequestParams();
-        $endpoint = $this->getApiPrefix().self::API_ENDPOINT.'/'.$definition['uuid'];
-        $response = $this->client->get($endpoint, $query);
-
-        $fields = $response['data']['attributes'];
-        $policies = Yaml::parse($fields['field_policies']);
-
-        foreach ($policies as $policy_name => $info) {
-            $severity = isset($info['severity']) ? Severity::fromValue($info['severity']) : Severity::getDefault();
-            $policies[$policy_name]['severity'] = $severity->value;
-        }
-
-        $definition = [
-          'title' => $fields['title'],
-          'name' => $fields['field_name'],
-          'uuid' => $definition['uuid'],
-          'language' => $definition['language'],
-          'description' => $fields['field_description'] ?? '',
-          'policies' => $policies,
-          'uri' => $definition['uri'],
-          'include' => $fields['field_include'],
-          'format' => [
-            'html' => [
-              'template' => $fields['field_html_template'] ?? '',
-              'content' => $fields['field_html_content'] ?? '',
-            ]
-          ]
-        ];
-
-        if (isset($fields['field_dependencies'])) {
-            $dependencies = Yaml::parse($fields['field_dependencies']);
-            foreach ($dependencies as $policy_name => $info) {
-                $severity = $info['severity'] ?? Severity::getDefault()->value;
-                $severity = is_numeric($severity) ? Severity::fromValue($severity)->value : Severity::from($severity)->value;
-                $dependencies[$policy_name]['severity'] = $severity;
-            }
-            $definition['dependencies'] = $dependencies;
-        }
-
-        try {
-            $definition['excluded_policies'] = !empty($fields['field_excluded_policies']) ? Yaml::parse($fields['field_excluded_policies']) : [];
-        } catch (ParseException $e) {
-        }
-
-        return parent::doLoad($definition);
-    }
 }
